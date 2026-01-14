c0c528af4a9be7b72e4e1c038240fdb0ba842714
mendezt01 <mendeztaniuska06@gmail.com>
2026-01-14 13:08:51 -0300

    Update main.py

--- main.py
+++ main.py
@@ -57,3 +57,8 @@ async def receive_event(request: Request):
         raise
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+
+        if __name__ == "__main__":
+            port = int(os.environ.get("PORT", 8080))
+            app.run(host="0.0.0.0", port=port)
